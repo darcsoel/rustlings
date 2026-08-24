f80fcefd5347ed29ad1b6d9a7659f2bb83ab3d13
ross <rostikvunarchuk@gmail.com>
2026-08-24 13:27:18 +0300

    enums; str; mod; hash; opt

--- exercises/quizzes/quiz2.rs
+++ exercises/quizzes/quiz2.rs
@@ -27,7 +27,9 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function as described above.
-    // pub fn transformer(input: ???) -> ??? { ??? }
+    pub fn transformer(input: vector()) -> vector(String) { 
+        
+     }
 }
 
 fn main() {
@@ -37,7 +39,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    // use ???;
+    use my_module;
     use super::Command;
 
     #[test]
